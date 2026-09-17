f698a655b49402bac57bf355f15812271eb6851f
jonjon-fr <315996245+jonjon-fr@users.noreply.github.com>
2026-09-17 09:42:52 -0300

    feat: adiciona complexidades dos casos

---
+++ src/main/java/br/com/fluxoestratificacao/model/ComplexidadeCaso.java
@@ -0,0 +1,10 @@
+package br.com.fluxoestratificacao.model;
+
+/**
+ * Representa as categorias fixas de complexidade aceitas pelo projeto.
+ */
+public enum ComplexidadeCaso { 
+    SIMPLES,
+    INTERMEDIARIO,
+    COMPLEXO;
+}
