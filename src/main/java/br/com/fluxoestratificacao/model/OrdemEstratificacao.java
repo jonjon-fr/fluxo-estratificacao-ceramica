88e038bce972e3d22a48227676b9858965639a0e
jonjon-fr <315996245+jonjon-fr@users.noreply.github.com>
2026-09-17 15:01:48 -0300

    style: organiza formatacao das classes principais

--- src/main/java/br/com/fluxoestratificacao/model/OrdemEstratificacao.java
+++ src/main/java/br/com/fluxoestratificacao/model/OrdemEstratificacao.java
@@ -12,28 +12,33 @@ public class OrdemEstratificacao {
     private ComplexidadeCaso complexidade;
     private LocalDate dataEntrada;
     private LocalDate previsaoConclusao;
-    
+
     public OrdemEstratificacao(
-       String  codigo,
-       ComplexidadeCaso  complexidade,
-       LocalDate  dataEntrada) {
-     this.codigo = codigo;
-     this.complexidade = complexidade;
-     this.dataEntrada = dataEntrada;     
+            String codigo,
+            ComplexidadeCaso complexidade,
+            LocalDate dataEntrada) {
+        this.codigo = codigo;
+        this.complexidade = complexidade;
+        this.dataEntrada = dataEntrada;
     }
+
     public String getCodigo() {
         return codigo;
-     }   
-     public void setPrevisaoConclusao(LocalDate previsaoConclusao) {
+    }
+
+    public void setPrevisaoConclusao(LocalDate previsaoConclusao) {
         this.previsaoConclusao = previsaoConclusao;
-     } 
-     public LocalDate getDataEntrada() {
-         return dataEntrada;
-     }   
-     public ComplexidadeCaso getComplexidade() {
-         return complexidade;
-     }
-     public LocalDate getPrevisaoConclusao() {
-         return previsaoConclusao; 
-     }    
+    }
+
+    public LocalDate getDataEntrada() {
+        return dataEntrada;
+    }
+
+    public ComplexidadeCaso getComplexidade() {
+        return complexidade;
+    }
+
+    public LocalDate getPrevisaoConclusao() {
+        return previsaoConclusao;
+    }
 }
--- src/main/java/br/com/fluxoestratificacao/service/PlanejadorProducao.java
+++ src/main/java/br/com/fluxoestratificacao/service/PlanejadorProducao.java
@@ -7,14 +7,18 @@ import br.com.fluxoestratificacao.strategy.EstrategiaPrazo;
  * Contexto do padrão Strategy: aplica a regra de prazo escolhida a uma ordem.
  */
 public class PlanejadorProducao {
+
     private EstrategiaPrazo estrategiaPrazo;
-public PlanejadorProducao(EstrategiaPrazo estrategiaPrazo) {
-    this.estrategiaPrazo = estrategiaPrazo;
+
+    public PlanejadorProducao(EstrategiaPrazo estrategiaPrazo) {
+        this.estrategiaPrazo = estrategiaPrazo;
     }
-public void definirEstrategia(EstrategiaPrazo estrategiaPrazo) {
-    this.estrategiaPrazo = estrategiaPrazo;
-    }    
-public void planejar(OrdemEstratificacao ordem) {
-    ordem.setPrevisaoConclusao(estrategiaPrazo.calcularPrevisao(ordem));
+
+    public void definirEstrategia(EstrategiaPrazo estrategiaPrazo) {
+        this.estrategiaPrazo = estrategiaPrazo;
+    }
+
+    public void planejar(OrdemEstratificacao ordem) {
+        ordem.setPrevisaoConclusao(estrategiaPrazo.calcularPrevisao(ordem));
     }
 }
